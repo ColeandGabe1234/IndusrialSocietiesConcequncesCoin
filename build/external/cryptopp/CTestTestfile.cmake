543a78b1d8c689d75c4887c93f98c52dce4cdac5
Ckaeboy22 <30983090+Ckaeboy22@users.noreply.github.com>
2023-01-25 15:48:13 -0500

    Delete build directory

--- build/external/cryptopp/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cole/Desktop/funny/tirtlecoin/external/cryptopp
-# Build directory: /home/cole/Desktop/funny/tirtlecoin/build/external/cryptopp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
